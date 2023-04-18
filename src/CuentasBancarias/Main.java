3e62ff3ce9277003e5d28b34b0fc22dd156e689d
andresred94 <bravocastro.andresmartin@gmail.com>
2023-04-18 09:57:39 +0200

    18/04/2023 - Probando a añadir cuenta bancaria y mostrarlo en pantalla

--- src/CuentasBancarias/Main.java
+++ src/CuentasBancarias/Main.java
@@ -3,6 +3,8 @@ package CuentasBancarias;
 public class Main {
     public static void main(String[] args) {
         CuentaBancaria cuenta1 = new CuentaBancaria();
-
+        cuenta1.setNumCuenta(355);
+        int c = cuenta1.getNumCuenta();
+        System.out.println("La cuenta es "+c);
     }
 }
