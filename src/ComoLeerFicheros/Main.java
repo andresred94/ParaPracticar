97ac5146ae29514bbc2b1d94f2599c335c6140b5
andresred94 <bravocastro.andresmartin@gmail.com>
2023-04-19 09:54:48 +0200

    19/04/2023 - Se crean dos TryCatch para leer y escribir un ficheros

--- src/ComoLeerFicheros/Main.java
+++ src/ComoLeerFicheros/Main.java
@@ -1,26 +1,33 @@
 package ComoLeerFicheros;
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
 public class Main {
     public static void main (String [] args){
-        /*FileWriter fw;
-        {
-            try {
-                fw = new FileWriter ("./src/fichero1.txt");
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-                }
-        }*/
-
-        try (FileWriter fw = new FileWriter("./src/ComoLeerFicheros/fichero1.txt")) {
+        /**
+         * Para poder leer un fichero usamos la clase FileWriter indicando en el constructor
+         * al momento de crear el objeto de la clase la ruta donde se encuentra el archivo.
+         * La ruta de este archivo tiene que tener la ruta relativa o absoluta.
+         */
+        //Try Catch para escribir
+        /*try (FileWriter fw = new FileWriter("./src/ComoLeerFicheros/fichero1.txt")) {
             fw.write("Esta es una prueba de como escribir en un archivo");
         } catch (IOException e) {
             System.out.println("Error al encontrar el arhivo");
-        }
-        try (FileWriter fw = new FileWriter("./src/ComoLeerFicheros/fichero1.txt")) {
-            fw.write("Esta es una prueba de como escribir en un archivo");
+        }*/
+
+        /**
+         * Para leer un archivo es casi exactamente igual que como se escribte pero cambiando
+         * a la clase FileReader con el metodo read().
+         */
+        //try cath para leer
+        try (FileReader fr = new FileReader("./src/ComoLeerFicheros/fichero1.txt")) {
+            int caracter;
+            while  ((caracter=fr.read()) != -1){
+                System.out.print((char)caracter);
+            }
         } catch (IOException e) {
             System.out.println("Error al encontrar el arhivo");
         }
