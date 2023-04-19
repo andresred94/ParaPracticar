3611e6986919dfc7d8d239e92193777ab38ab709
andresred94 <bravocastro.andresmartin@gmail.com>
2023-04-19 10:40:08 +0200

    19/04/2023 - añadido un manera de leer el contenido de un directorio (sin implementar en una clas)

--- src/ComoLeerFicheros/Main.java
+++ src/ComoLeerFicheros/Main.java
@@ -11,25 +11,52 @@ public class Main {
          * al momento de crear el objeto de la clase la ruta donde se encuentra el archivo.
          * La ruta de este archivo tiene que tener la ruta relativa o absoluta.
          */
-        //Try Catch para escribir
-        try (FileWriter fw = new FileWriter("./src/ComoLeerFicheros/fichero1.txt")) {
+        //Try Catch para escribir en un fichero
+        /*try (FileWriter fw = new FileWriter("./src/ComoLeerFicheros/fichero1.txt")) {
             fw.write("Esta es una prueba de como escribir en un archivo");
+            fw.write(" ");
+            fw.write("Es una prueba con caracteres");
+            fw.write(" ");
+            fw.write("aunque un string tambien tiene  numero");
+            fw.write(" ");
+            fw.write("1");
+
         } catch (IOException e) {
             System.out.println("Error al encontrar el arhivo");
-        }
+        }*/
 
         /**
          * Para leer un archivo es casi exactamente igual que como se escribte pero cambiando
          * a la clase FileReader con el metodo read().
          */
-        //try cath para leer
-        try (FileReader fr = new FileReader("./src/ComoLeerFicheros/fichero1.txt")) {
+        //try cath para leer un fichero
+        /*try (FileReader fr = new FileReader("./src/ComoLeerFicheros/fichero1.txt")) {
             int caracter;
             while  ((caracter=fr.read()) != -1){
                 System.out.print((char)caracter);
             }
         } catch (IOException e) {
-            System.out.println("Error al encontrar el arhivo");
+            System.out.println("Error al encontrar el archivo");
+        }*/
+
+        /**
+         * Para poder leer el contenido de un  directorio se utiliza una clase diferente
+         * En este caso usamos la clase "File" y los metodos exists() y "isDirectory"
+         * para comprobar si existe el directorio y si es un directorio
+         * Para leer cada archivo o capreta que tenga el directorio usamos un bucle
+         * for-each para almacenar los archivos en un array, por último para obtener el nombre
+         * cada archivo usamos el metodo "getName()" para obtener el nombre de cada arhivo dentro
+         * del array
+         */
+        // Como leer el contenido de una carpeta
+        File dr = new File("D:\\");
+        if (dr.exists() && dr.isDirectory()){
+            File [] files = dr.listFiles();
+            for (File archivo: files) {
+                System.out.println(archivo.getName());
+            }
+        } else {
+            System.out.println("No se encuentra el directorio");
         }
     }
 
---
+++ src/ComoLeerFicheros/ManejoFichero.java
@@ -0,0 +1,11 @@
+package ComoLeerFicheros;
+
+import com.sun.source.tree.BreakTree;
+
+import java.util.List;
+
+public class ManejoFichero {
+    /*public List<String> listarDirectorio (String ruta){
+        return List<String>;
+    } */
+}
